e34fe11b95677ef3d6736c81f548f799dcb0556b
LevsJuarez1 <levsjuarez@gmail.com>
2016-09-01 21:06:10 -0500

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,10 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> var1 = Hello World!
+SyntaxError: invalid syntax
+>>> var1 = "Hello World"
+>>> print var1
+SyntaxError: Missing parentheses in call to 'print'
+>>> print (var1)
+Hello World
+>>> 
